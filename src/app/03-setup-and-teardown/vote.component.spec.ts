e05ee12c55abb0034f90040613686bd3cbb891ca
Rafael <rreynoud@gmail.com>
2018-05-19 16:50:40 -0300

    Funcionando testes

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,37 @@
+import { Component } from '@angular/core';
 import { VoteComponent } from './vote.component'; 
 
+//Arrange
+let component: VoteComponent;
+
+beforeEach(()=>{
+  component = new VoteComponent();
+});
+
 describe('VoteComponent', () => {
-  it('', () => {
+  it('should increment totalVotes when upvoted', () => {
+ 
+
+    //afterEach(() => {});
+    // beforeAll();
+    // afterAll();
+
+    // Act
+    component.upVote();
+
+    //Assert
+    expect(component.totalVotes).toBe(1);
   });
 
-  it('', () => {
+  it('should decrement totalVotes when downvotes', () => {
+    //Arrange
+    let component = new VoteComponent();
+    
+    // Act
+    component.downVote();
+
+    //Assert
+    expect(component.totalVotes).toBe(-1);
   });
+
 });
